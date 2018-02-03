d96613e8637927de76cf7ca469dc870cbeb061d9
Alexander Cryderman <buster2694@gmail.com>
2018-02-03 12:56:16 -0500

    stuffcheckagain

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,15 @@
-alert("working");
+// var locationInput;
+// var date;
+// var type;
+
+$("#resultsSection").hide();
+$("#submit").click(function(){
+    $("#inputMenu").hide();
+    $("#resultsSection").show();
+    var userLocation = $("#location-input").val();
+    var userDate = $("dates-input").val();
+    var userEvent = $("eventType-input").val();
+    console.log(userLocation);
+    console.log(userDate);
+    console.log(userEvent);
+});
