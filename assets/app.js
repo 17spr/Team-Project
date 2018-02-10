dacf9b1a8e28c01c81c495583e69463bb59ca009
Alexander Cryderman <buster2694@gmail.com>
2018-02-10 09:54:22 -0500

    Works 100%

--- assets/app.js
+++ assets/app.js
@@ -1,82 +1,22 @@
-<<<<<<< HEAD
-var postalCode
-var userzip= $("location-input").val();
-var keyword
-
-
-$("#resultsSection").hide();
-$("#submit").click(function () {
-    $("#inputMenu").hide();
-    $("#resultsSection").show();
-    // userLocation = $("#location-input").val();
-    postalCode = $("#location-input").val();
-    console.log(postalCode);
-    keyword = $("#eventType-input").val();
-
-    $("#map-container").attr("src", "https://www.google.com/maps/embed/v1/place?q="+ postalCode +"&key=AIzaSyAHuXEAS9mdigA2s8g5qz323VNGt70mpbk")
-
-    $.ajax({
-        type: "GET",
-        url: "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + keyword + "&postalCode=" + postalCode + "&size=10&apikey=3YaVJZm3QgMON91AlLZSyPWcb28zycgA",
-        async: true,
-        dataType: "json",
-        success: function (json) {
-            console.log(json);
-
-            for (var i = 0; i < 10; i++) {
-                var eventName = json._embedded.events[i].name;
-                var eventImage = json._embedded.events[i].images[1].url;
-                var eventNote = json._embedded.events[i].pleaseNote;
-                var eventTickets = json._embedded.events[i].url;
-                var eventAddress= json._embedded.events[i]._embedded.venues[0].address.line1;
-                console.log("ADDRESS = " + eventAddress);
-                //stops displaying undefined
-                if (eventNote === undefined){
-                    eventNote = "No notes available for this event";
-                };
-                $("#events").append("<p class = 'well'>" + eventName
-                    + "<br>"+
-                    "<img src=" + eventImage + " width=300 height=200>"
-                    + "<br>" +
-                    "Please note: "+ eventNote
-                    +"<br>" +
-                    "<a href=" + eventTickets +">" + "Buy your tickets here!</a>" + "</p>")
-
-            }
-
-        },
-        error: function (xhr, status, err) {
-            console.log("nope");
-            // This time, we do not end up here!
-        }
-    });
-
-    
-
-});
-=======
 $(document).ready(function () {
-
+    AOS.init();
     var postalCode
     var keyword
-
     $("#resultsSection").hide();
     $("#submit").on('click', function (e) {
         e.preventDefault();
+        $("#location-input").text("");
         postalCode = $("#location-input").val().trim();
         $("#events").empty();
-
         if (postalCode == '') {
             $("#reqTarget").prepend("This field is required");
         } else {
-
-            // $("#inputMenu").hide();
+            $("#inputMenu").hide();
             $("#resultsSection").show();
-            // userLocation = $("#location-input").val();
             postalCode = $("#location-input").val();
             console.log(postalCode);
             keyword = $("#eventType-input").val();
-            $("#map-container").attr("src", "https://www.google.com/maps/embed/v1/place?q="+ postalCode +"&key=AIzaSyAHuXEAS9mdigA2s8g5qz323VNGt70mpbk")
+
             $.ajax({
                 type: "GET",
                 url: "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + keyword + "&postalCode=" + postalCode + "&size=10&apikey=3YaVJZm3QgMON91AlLZSyPWcb28zycgA",
@@ -86,43 +26,44 @@ $(document).ready(function () {
                     console.log(json);
                     if (json._embedded) {
                         for (var i = 0; i < 10; i++) {
-                            var eventName = json._embedded.events[i].name;
-                            var eventImage = json._embedded.events[i].images[1].url;
-                            var eventNote = json._embedded.events[i].pleaseNote;
-                            var eventTickets = json._embedded.events[i].url;
-                            var eventAddress = json._embedded.events[i]._embedded.venues[0].address.line1;
-                            console.log("ADDRESS = " + eventAddress);
-                            //stops displaying undefined
+                            var responseData = json._embedded.events[i];
+                            var eventName = responseData.name;
+                            var eventImage = responseData.images[1].url;
+                            var eventNote = responseData.pleaseNote;
+                            var eventTickets = responseData.url;
+                            var eventAddress = responseData._embedded.venues[0].address.line1;
+                            var eventLat = responseData._embedded.venues[0].location.latitude;
+                            var eventLong = responseData._embedded.venues[0].location.longitude;
+
                             if (eventNote === undefined) {
                                 eventNote = "No notes available for this event";
                             };
->>>>>>> 483f8f025b695f95e5951cc933800ac1c80c68cd
-
 
                             $("#events").attr("class", "col-md-6")
-                                .append("<p class = 'well'>" + eventName
+                                .append("<p data-aos='fade-right' class = 'well'>" + eventName
                                 + "<br>" +
                                 "<img id='imgAPI' src=" + eventImage + " width=300 height=200>"
                                 + "<br>" +
                                 "Please note: " + eventNote
                                 + "<br>" +
                                 "<a href=" + eventTickets + " target='_blank'>" + "Buy your tickets here!</a>" + "</p>"
-                                //GOOGLE MAPS HERE                        
                                 )
-
-                        }
+                            $("#map-container").attr("src", "https://www.google.com/maps/embed/v1/place?q=" + postalCode + "&key=AIzaSyAHuXEAS9mdigA2s8g5qz323VNGt70mpbk")
+                          
+                        };
                     } else {
                         $("#events").attr("class", "well")
-                            .append("Sorry there are no shows in your area");
+                            .append("<p>Sorry there are no shows in your area</p>")
+                            .append("<br>")
+                            .append("<button id='reloadButton' class='noShowButton'>Search Again</button>");
                     }
-
+                    $("#reloadButton").click(function(){
+                        location.reload();
+                    })
                 },
                 error: function (xhr, status, err) {
-                    console.log("nope");
-                    // This time, we do not end up here!
                 }
             });
-
         }
     });
 });
